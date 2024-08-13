9058c6a0e45174915b999c5bc6af1a86f7e4acfb
oyrange <lojewski@berkeley.edu>
2024-08-13 02:20:46 -0700

    Update movie controller index to show only possible ratings

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,9 @@ class Movie < ActiveRecord::Base
   def new
     @possible_ratings = (%w[G PG PG-13 R])
   end
+
+  def self.get_movies_rated(ratings)
+    ratings = [ratings] unless ratings.is_a?(Array)
+    where(rating: ratings)
+  end
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require 'spec_helper'
-
 
 describe Movie do
   it 'should understand possible ratings' do
     expect(Movie.possible_ratings).to eq(%w[G PG PG-13 R])
+  end
+
+  it 'should be able to get movies with specified ratings' do
+    r_movie_search = Movie.get_movies_rated('R')
+    expect(r_movie_search.rating.uniq.count).to eq(1)
+    expect(r_movie_search.first.rating).to eq('R')
   end
 end
